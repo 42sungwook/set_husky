158e7462732a941e6c471355f6df1b0b55ade54c
Sungwook Kim <sungwook@c1r7s2.42seoul.kr>
2023-07-16 13:15:49 +0900

    [Edit] shell file

--- test.sh
+++ test.sh
@@ -9,23 +9,23 @@ commit_msg_title=\$(head -n 1 \"\$1\")
 commit_msg_body=\$(tail -n +2 \"\$1\")
 
 # Regular expression pattern to match specific commit types at the beginning of the commit message
-commit_type_regex=\"^(\\[(Feat|Add|Delete)\\])\"
+commit_type_regex=\"^(\\[(Add|Edit|Fix|Test|Delete|Doc)\\])\"
 
-if [[ \$commit_msg_title =~ \$commit_type_regex ]]; then
-	commit_type=\"\${BASH_REMATCH[1]}\"
-	echo \"\$commit_type \$commit_msg_title\" > \"\$1\"
-else
-	echo \"Invalid commit type. Available commit types: [Feat], [Add], [Delete]\"
+if [[ ! \$commit_msg_title =~ \$commit_type_regex ]]; then
+	red=\$(tput setaf 1)
+	reset=\$(tput sgr0)
+	echo -e \"\${red}Invalid commit type. Available commit types: [Add], [Edit], [Fix], [Test], [Delete], [Doc]\${reset}\"
 	exit 1
 fi
-
-if [[ -n \$commit_msg_body ]]; then
-	echo \"\$commit_msg_body\" >> \"\$1\"
-fi
 "
+
+if [ -f "$prepare_commit_msg_path" ]; then
+  rm "$prepare_commit_msg_path"
+  echo -e "$(tput setaf 1)Deleted existing prepare-commit-msg file$(tput sgr0)"
+fi
 
 # Create the prepare-commit-msg file
 echo "$prepare_commit_msg_content" > "$prepare_commit_msg_path"
 chmod +x "$prepare_commit_msg_path"
 
-echo "prepare-commit-msg file has been added to .git/hooks folder."
+echo -e "$(tput setaf 2)prepare-commit-msg file has been added to .git/hooks folder.$(tput sgr0)"
